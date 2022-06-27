31ef7ca5b8840b517b3c63d62c48ddab47f367ce
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-06-27 23:14:54 +0530

    üser action and reducer has created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import {BrowserRouter , Route , Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import About from "./components/DummyRoutes/About";
 import Contact from "./components/DummyRoutes/Contact";
-import TermsPolicy  from "./components/DummyRoutes/TermsPolicy";
+import TermsPolicy from "./components/DummyRoutes/TermsPolicy";
 import NavBar from "./components/NavBar";
 import HomePage from "./Pages/HomePage";
 import CartPage from "./Pages/CartPage";
 import LoginPage from "./Pages/LoginPage";
 import SignUpPage from "./Pages/SignUpPage";
+import OrderPage from "./Pages/OrderPage";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/policy" component={TermsPolicy} exact />
         <Route path="/" component={HomePage} exact />
         <Route path="/cart" component={CartPage} />
+        <Route path="/order" component={OrderPage} />
         <Route path="/login" component={LoginPage} exact />
         <Route path="/signup" component={SignUpPage} exact />
       </Switch>
--- src/Redux/reducers/OrderReducer.js
+++ src/Redux/reducers/OrderReducer.js
@@ -19,3 +19,25 @@ export const PlaceOrderReducer = (state = {}, action) => {
       return state;
   }
 };
+
+
+export const getUserOrdersReducer = (state ={orders: []}, action) => {
+  switch (action.type) {
+    case "USER_ORDER_REQUEST":
+      return {
+        loading:true
+      }
+    case "USER_ORDER_SUCCESS":
+      return {
+        loading: false,
+        success: true,
+        orders:action.payload
+      }
+    case "USER_ORDER_FAIL":
+      return {
+        loading: false,
+        error:action.payload
+      }
+    default : return state
+  }
+}
