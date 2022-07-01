11d397e099ac0573f05e2f686fa8140bf9a1ffa6
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-07-01 23:55:30 +0530

    roouting done for admin dashbord

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -1,32 +1,80 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Row, Col, Container, ButtonGroup, Button } from "react-bootstrap"
+import { useSelector} from "react-redux"
 import AddNewPizzasAdmin from '../components/admin/AddNewPizzasAdmin';
 import OrderListAdmin from '../components/admin/OrderListAdmin';
 import PizzasListAdmin from '../components/admin/PizzasListAdmin';
 import UserLisAdmint from '../components/admin/UserLisAdmint';
 import {Switch , Route} from "react-router-dom"
 
-const AdminPage = () => {
+
+const AdminPage = ({ history }) => {
+    
+    const userState = useSelector((state) => state.loginUserReducer);
+    const { currentUser } = userState;
+   console.log(currentUser);
+
+    
+    useEffect(() => {
+        if (
+          localStorage.getItem("currentUser") === null ||
+          !currentUser.isAdimn
+        ) {
+          window.location.href = "/";
+        }
+    },[])
   return (
     <>
       <Container>
         <Row>
-          <h1 className='text-center bg-dark text-light p2'>Admin Dashboard</h1>
+          <h1 className="text-center bg-dark text-light p2">Admin Dashboard</h1>
           <Col md={4}>
-            <ButtonGroup vertical style={{minHeight:"250px"}}>
-              <Button className='mb-2' >All Users</Button>
-              <Button variant='success' className='mb-2'>All Pizzas</Button>
-              <Button variant='info' className='mb-2'>Add Pizzas</Button>
-              <Button variant="warning" className='mb-2'>All Orders</Button>
+            <ButtonGroup vertical style={{ minHeight: "250px" }}>
+              <Button
+                onClick={() => history.push("/admin/userlist")}
+                className="mb-2"
+              >
+                All Users
+              </Button>
+              <Button
+                onClick={() => history.push("/admin/pizzalist")}
+                variant="success"
+                className="mb-2"
+              >
+                All Pizzas
+              </Button>
+              <Button
+                onClick={() => history.push("/admin/addnewpizza")}
+                variant="info"
+                className="mb-2"
+              >
+                Add Pizzas
+              </Button>
+              <Button
+                onClick={() => history.push("/admin/orderlist")}
+                variant="warning"
+                className="mb-2"
+              >
+                All Orders
+              </Button>
             </ButtonGroup>
           </Col>
-                  <Col md={8}>
-                      <Switch>
-                          <Route path="/admin/addnewpizza" component={AddNewPizzasAdmin} />
-                          <Route path="/admin/orderlist" component={OrderListAdmin} />
-                          <Route path="/admin/pizzalist" component={PizzasListAdmin} />
-                          <Route path="/admin/userlist" component={UserLisAdmint} />
-                      </Switch>    
+          <Col md={8}>
+            <Switch>
+              <Route path="/admin" component={UserLisAdmint} exact />
+              <Route
+                path="/admin/addnewpizza"
+                component={AddNewPizzasAdmin}
+                exact
+              />
+              <Route path="/admin/orderlist" component={OrderListAdmin} exact />
+              <Route
+                path="/admin/pizzalist"
+                component={PizzasListAdmin}
+                exact
+              />
+              <Route path="/admin/userlist" component={UserLisAdmint} exact />
+            </Switch>
           </Col>
         </Row>
       </Container>
--- src/Redux/actions/UserAction.jsx
+++ src/Redux/actions/UserAction.jsx
@@ -26,5 +26,5 @@ export const loginUser = (user) => async (dispatch) => {
 
 export const logoutUser = () => dispatch => {
     localStorage.removeItem("currentUser")
-    window.location.href = "/login"
+    window.location.href = "/"
 }
